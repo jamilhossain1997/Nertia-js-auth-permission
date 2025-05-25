906f985de954a35d92d263f5415dc85eb47b1763
Md. Jamil Hossain || AP || WA <jamil.hossain@AP-Jamil-WA.btraccl.com>
2025-05-25 13:48:16 +0600

    Role Controller

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class RoleController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Roles/Index', [
+            'roles' => Role::with('permissions')->get(),
+            'permissions' => Permission::all(),
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['name' => 'required|unique:roles']);
+        $role = Role::create(['name' => $request->name]);
+        $role->syncPermissions($request->permissions ?? []);
+        return redirect()->back();
+    }
+
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return redirect()->back();
+    }
+}
